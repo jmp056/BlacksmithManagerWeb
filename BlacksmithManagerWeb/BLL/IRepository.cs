afeab4852f8c7060a553ac2b63dd5587fc611099
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-01-28 19:35:40 -0400

    Agregando IRepository al proyecto!

---
+++ BlacksmithManagerWeb/BLL/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace BlacksmithManagerWeb.BLL
+{
+    interface IRepository<T> where T:class
+    {
+        public bool Guardar(T entity);
+        public bool Modificar(T entity);
+        public bool Eliminar(T entity);
+        public T Buscar(int Id);
+        public List<T> GetList(Expression<Func<T, bool>> expression);
+    }
+}
