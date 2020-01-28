5c68cfb78176c29de757384e8ed5c19792b71984
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-01-27 20:46:02 -0400

    Creando la clase contexto

---
+++ BlacksmithManagerWeb/Data/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlacksmithManagerWeb.Data
+{
+    public class Contexto : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=JMP-PC\SQLEXPRESS; Database = Personas; Trusted_Connection =true");
+        }
+    }
+}
